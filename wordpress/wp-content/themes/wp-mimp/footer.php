0d0630c7a7baf622fce12330db3c4b11680fc4dc
GrilloPress <grillopress@gmail.com>
2015-05-31 14:49:03 +0000

    update footer classes

--- wordpress/wp-content/themes/wp-mimp/footer.php
+++ wordpress/wp-content/themes/wp-mimp/footer.php
@@ -1,29 +1,36 @@
-			<footer role="contentinfo">
+</div> <!-- end #container -->			
+
+<footer role="contentinfo">
 			
-				<div id="inner-footer" class="clearfix">
-		          <hr />
-		          <div id="widget-footer" class="clearfix row">
+        <section class="main-footer">
+          <div class="container">
+            <div class="row">
+              <div id="widget-footer" class="clearfix row">
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
 		            <?php endif; ?>
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
 		            <?php endif; ?>
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
 		            <?php endif; ?>
-		          </div>
-					
-					<nav class="clearfix">
-						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-					</nav>
-					
-					<p class="pull-right"><a href="#" title="Back to the top">back to the top</a></p>
-			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
-				
-				</div> <!-- end #inner-footer -->
-				
+		          </div>  
+            </div>            
+          </div>
+        </section>
+        
+        
+        <section class="sub-footer">
+          <div class="container">
+            <div class="row">
+              <div class="clearfix">
+                <p class="pull-right"><a href="#" title="Back to the top">back to the top</a></p>
+                <p class="attribution">&copy; <?php bloginfo('name'); ?></p>
+              </div>
+            </div>
+          </div>
+        </section>
 			</footer> <!-- end footer -->
 		
-		</div> <!-- end #container -->
+		
 				
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
@@ -31,10 +38,6 @@
 		<![endif]-->
 		
 		<?php wp_footer(); // js scripts are inserted using this function ?>
-
-		<!-- remove this for production -->
-
-		<script src="//localhost:35729/livereload.js"></script>
 
 	</body>
 
