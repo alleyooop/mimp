f2350889bfc4cc974b0320e3e9e4d855070a6dea
GrilloPress <grillopress@gmail.com>
2015-06-01 12:47:37 +0000

    create homepage template

--- wordpress/wp-content/themes/wp-mimp/page-homepage.php
+++ wordpress/wp-content/themes/wp-mimp/page-homepage.php
@@ -6,77 +6,79 @@ Template Name: Homepage
 
 <?php get_header(); ?>
 			
-			<div id="content" class="clearfix row">
+
+      <!-- hack -->
+        </div>
+      <!-- hack -->
+
+			<div id="content">
 			
-				<div id="main" class="col-sm-12 clearfix" role="main">
+				<div id="main" class="clearfix" role="main">
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-					
-						<header>
+					<section id="intro">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="brand-header">
+                    <h1 class="brand-h1">Medical Imaging &amp; Medical Physics</h1>
+                    <p class="lead">A multi-disciplinary department in the heart of South Yorkshire</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 
-							<?php 
-								$post_thumbnail_id = get_post_thumbnail_id();
-								$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpbs-featured-home' );
-							?>
+          <section id="mimp-blocks">
+            <div class="container">
+              <div class="row">
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <a href="#">
+                      <img src="<?php echo content_url('/themes/wp-mimp/images/radiology-reception.jpg'); ?>" class="hidden-xs" width="100%">
+                      <div class="well" id="">
+                        <h4>Patient Information</h4>
+                        <footer>Learn more about your patient journey and our services</footer>
+                      </div>
+                    </a>
+                  </div>
 
-							<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-				
-								<div class="page-header">
-									<h1><?php bloginfo('title'); ?><small><?php echo get_post_meta($post->ID, 'custom_tagline' , true);?></small></h1>
-								</div>				
-								
-							</div>
-						
-						</header>
-						
-						<section class="row post_content">
-						
-							<div class="col-sm-8">
-						
-								<?php the_content(); ?>
-								
-							</div>
-							
-							<?php get_sidebar('sidebar2'); // sidebar 2 ?>
-													
-						</section> <!-- end article header -->
-						
-						<footer>
-			
-							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					<?php 
-						// No comments on homepage
-						//comments_template();
-					?>
-					
-					<?php endwhile; ?>	
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <a href="#" class="hidden-xs" width="100%">
+                      <img src="<?php echo content_url('/themes/wp-mimp/images/services-stock.jpg'); ?>" class="hidden-xs" width="100%">
+                      <div class="well" id="">
+                        <h4>A-Z of Services</h4>
+                        <footer>Explore our commercial and patient services</footer>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <a href="#" class="hidden-xs" width="100%">
+                      <img src="<?php echo content_url('/themes/wp-mimp/images/commercial.jpg'); ?>" class="hidden-xs" width="100%">
+                      <div class="well" id="">
+                        <h4>Commercial Services</h4>
+                        <footer>Find out more about our world-class commerical services</footer>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div class="col-md-3 col-sm-6 col-xs-12">
+                    <a href="#" class="hidden-xs" width="100%">
+                      <img src="<?php echo content_url('/themes/wp-mimp/images/doctor.jpg'); ?>" class="hidden-xs" width="100%">
+                      <div class="well" id="">
+                        <h4>Referral Information</h4>
+                        <footer>Find out how to make a referral to our service</footer>
+                      </div>
+                    </a>
+                  </div>
+              </div>
+            </div>
+          </section>
 			
 				</div> <!-- end #main -->
     
-				<?php //get_sidebar(); // sidebar 1 ?>
-    
 			</div> <!-- end #content -->
+
+      <div class="container">
+       
 
 <?php get_footer(); ?>
