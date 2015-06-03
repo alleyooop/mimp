fd790ac6776914c27c76cddd8f314c6147a4a48c
GrilloPress <grillopress@gmail.com>
2015-06-03 10:50:52 +0000

    add team section to single templates

--- wordpress/wp-content/themes/wp-mimp/single-services.php
+++ wordpress/wp-content/themes/wp-mimp/single-services.php
@@ -31,11 +31,14 @@
 				  </article> <!-- end article -->
           
           <section class="team">
-            <?php if(get_field('mimp_team')) : ?>
-              <div class="well">
-              <?php the_field('mimp_team'); ?>
-              </div>
-            <?php endif; ?>
+            <div class="well">
+              <?php if(get_field('field_name')){
+                
+                echo the_field('mimp_team');
+  
+               } ?>
+              
+            </div>
           </section>
         </div>
         
@@ -43,7 +46,7 @@
                   
           <section class="well">
             <h4>Contact us</h4>
-            <p>If you have any questions please feel free to contact the department via the methods below</p>
+            <p>If you have any questions please feel free to contact the service via the methods below</p>
             <a href="mailto:<?php the_field('mimp_contact_email'); ?>" class="btn btn-warning" alt="email address">Email us</a>
             <a href="tel:<?php the_field('mimp_contact_number'); ?>" class="btn btn-link" alt="Radiography's telephone number">Call us</a>
           </section>
