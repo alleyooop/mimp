9df6ed162022110133e0d05b34e931402f8bbc21
GrilloPress <grillopress@gmail.com>
2015-06-04 13:08:39 +0000

    update fontpage typography and responsive styles

--- wordpress/wp-content/themes/wp-mimp/page-homepage.php
+++ wordpress/wp-content/themes/wp-mimp/page-homepage.php
@@ -35,17 +35,17 @@ Template Name: Homepage
                     <a href="<?php echo home_url( '/patient-information/' ); ?>">
                       <img src="<?php echo content_url('/themes/wp-mimp/images/radiology-reception.jpg'); ?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
-                        <h4>Patient Information</h4>
+                        <h2>Patient Information</h2>
                         <footer>Learn more about your patient journey and our services</footer>
                       </div>
                     </a>
                   </div>
 
                   <div class="col-md-3 col-sm-6 col-xs-12">
-                    <a href="<?php echo home_url( '/departments-and-services/' ); ?>" class="hidden-xs" width="100%">
+                    <a href="<?php echo home_url( '/departments-and-services/' ); ?>">
                       <img src="<?php echo content_url('/themes/wp-mimp/images/services-stock.jpg'); ?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
-                        <h4>A-Z of Services</h4>
+                        <h2>A-Z of Services</h2>
                         <footer>Explore our commercial and patient services</footer>
                       </div>
                     </a>
@@ -55,17 +55,17 @@ Template Name: Homepage
                     <a href="#" class="hidden-xs" width="100%">
                       <img src="<?php echo content_url('/themes/wp-mimp/images/commercial.jpg'); ?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
-                        <h4>Commercial Services</h4>
+                        <h2>Commercial Services</h2>
                         <footer>Find out more about our world-class commerical services</footer>
                       </div>
                     </a>
                   </div>
 
                   <div class="col-md-3 col-sm-6 col-xs-12">
-                    <a href="#" class="hidden-xs" width="100%">
+                    <a href="<?php echo home_url( '/referral-information/' ); ?>">
                       <img src="<?php echo content_url('/themes/wp-mimp/images/doctor.jpg'); ?>" class="hidden-xs" width="100%">
                       <div class="well" id="">
-                        <h4>Referral Information</h4>
+                        <h2>Referral Information</h2>
                         <footer>Find out how to make a referral to our service</footer>
                       </div>
                     </a>
